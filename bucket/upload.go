de692263585b3be4274eddf1bde28cb4395579d1
empirefox <empirefoxit@sina.com>
2015-07-09 14:29:46 +0800

    add err log to client

--- bucket/upload.go
+++ bucket/upload.go
@@ -1,7 +1,6 @@
 package bucket
 
 import (
-	"io"
 	"mime/multipart"
 	"net/http"
 	"strings"
@@ -89,23 +88,42 @@ type UploadRetJsonp struct {
 	UpJson
 }
 
+func resUpJson(w http.ResponseWriter, ret interface{}) {
+	err := MsgJsonpTpl.Execute(w, ret)
+	if err != nil {
+		glog.Infoln("模板调用错误:", err)
+	}
+}
+
+func (ret *UploadRetJsonp) Respose(w http.ResponseWriter, url string) {
+	ret.Url = url
+	resUpJson(w, ret)
+}
+
+func (ret *UploadRetJsonp) Err(w http.ResponseWriter, err error) {
+	ret.Error = 1
+	ret.Message = err.Error()
+	resUpJson(w, ret)
+}
+
 //martini Handler
 func UploadHandlers() []martini.Handler {
 	var bind martini.Handler = binding.MultipartForm(UploadData{})
 	var upload = func(data UploadData, w http.ResponseWriter) {
 		retJsonp := &UploadRetJsonp{MessengerJs: MessengerJs, UpTime: data.UpTime}
 
-		defer func() {
-			if err := recover(); err != nil {
-				retJsonp.Error = 1
-				retJsonp.Message = "上传图片错误！"
-				resUpJson(w, retJsonp)
-			}
-		}()
-
 		//取得bucket
 		bucket, err := bucketdb.FindByName(data.Bucket)
-		panicErr(err)
+		if err != nil {
+			retJsonp.Err(w, err)
+			return
+		}
+
+		imgFile, err := data.ImgFile.Open()
+		if err != nil {
+			retJsonp.Err(w, err)
+			return
+		}
 
 		//上传内容到Qiniu
 		var ret qio.PutRet
@@ -114,38 +132,16 @@ func UploadHandlers() []martini.Handler {
 		// key:imgName	为文件存储的标识
 		// r:imgFile   	为io.Reader类型，用于从其读取数据
 		// extra     	为上传文件的额外信息,可为空， 详情见 qio.PutExtra, 可选
-		var imgFile io.Reader
-		imgFile, err = data.ImgFile.Open()
-		panicErr(err)
 		err = qio.Put(nil, &ret, bucket.Uptoken, data.ImgName(), imgFile, nil)
-		logPanicErr(bucket, err)
+		if err != nil {
+			bucket.LogErr()
+			retJsonp.Err(w, err)
+			return
+		}
 
 		//上传成功，返回给KindEditor
 		//w.Header().Set("Content-type", "application/json")
-		retJsonp.Url = bucket.ImgUrl(ret.Key)
-		resUpJson(w, retJsonp)
+		retJsonp.Respose(w, bucket.ImgUrl(ret.Key))
 	}
 	return []martini.Handler{bind, upload}
-}
-
-func resUpJson(w http.ResponseWriter, ret interface{}) {
-	err := MsgJsonpTpl.Execute(w, ret)
-	if err != nil {
-		glog.Infoln("模板调用错误:", err)
-	}
-}
-
-//如果有错误则记录err到数据库，并panic
-func logPanicErr(b *bucketdb.Bucket, err error) {
-	if err != nil {
-		b.LogErr()
-		glog.Errorln("上传错误:", err)
-		panic(err)
-	}
-}
-
-func panicErr(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
